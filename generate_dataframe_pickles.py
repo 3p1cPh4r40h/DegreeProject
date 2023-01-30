a2168156a982456db603997efa57cff74bc73e80
3p1cPh4r40h <russaz09@hotmail.com>
2023-01-29 19:56:26 -0500

    change to pkl file generation
    Audio split into even chunks for processing and training purposes.

--- generate_dataframe_pickles.py
+++ generate_dataframe_pickles.py
@@ -1,32 +1,52 @@
+# Importing required libraries
 import os
 import librosa # for audio processing
-import matplotlib.pyplot as plt # used with librosa for data visualization
 import pandas as pd # for dataframe
-import numpy as np # used for math and data structures
-from librosa import display # used to display files imported from librosa
+import numpy as np # used for mathematical operations and data structures
 
-# create empty lists for Test and Training
+# Creating empty lists for Test and Training datasets
 test_list = []
 train_list = []
 
-# iterate over Test and Training Folders
-for folder in ['Test', 'Training']:
-    # iterate over subfolders
-    for subfolder in os.listdir(folder):
-        # iterate over audio files in subfolder
-        for file in os.listdir(f'{folder}/{subfolder}'):
-            # load audio file
-            audio, sr = librosa.load(f'{folder}/{subfolder}/{file}')
-            # create new row in appropriate list
-            if folder == 'Test':
-                test_list.append({'audio': audio, 'chord': subfolder, 'sr': sr})
-            else:
-                train_list.append({'audio': audio, 'chord': subfolder, 'sr': sr})
+# Length of the shortest audio clip in the dataset
+smallest_shape = 24775
 
-# create dataframes from lists
+# Iterating over the 'Test' and 'Training' folders
+for folder in ['Test', 'Training']:
+    # Iterating over sub-folders within the folder
+    for subfolder in os.listdir(folder):
+        # Iterating over audio files in the subfolder
+        for file in os.listdir(f'{folder}/{subfolder}'):
+            # Loading the audio file
+            audio, sr = librosa.load(f'{folder}/{subfolder}/{file}')
+            
+            # If the length of the audio clip is greater than the smallest shape
+            if audio.shape[0] > smallest_shape:
+                # Cutting the audio into as many pieces of the smallest shape as possible, discarding the rest
+                num_pieces = audio.shape[0] // smallest_shape
+                audio = audio[:num_pieces * smallest_shape]
+                audio = audio.reshape(-1, smallest_shape)
+                
+                # Appending each piece to the appropriate list
+                for piece in audio:
+                    if folder == 'Test':
+                        test_list.append({'audio': piece, 'chord': subfolder, 'sr': sr})
+                    else:
+                        train_list.append({'audio': piece, 'chord': subfolder, 'sr': sr})
+            else:
+                # Appending the audio clip to the appropriate list
+                if folder == 'Test':
+                    test_list.append({'audio': audio, 'chord': subfolder, 'sr': sr})
+                else:
+                    train_list.append({'audio': audio, 'chord': subfolder, 'sr': sr})
+
+# Creating dataframes from the lists
 test_df = pd.DataFrame(test_list, columns=['audio', 'chord', 'sr'])
 train_df = pd.DataFrame(train_list, columns=['audio', 'chord', 'sr'])
 
+# Saving the dataframes to disk as .pkl files
 test_df.to_pickle('test_df.pkl')
 train_df.to_pickle('train_df.pkl')
 
+# Printing a message indicating the dataframes have been saved successfully
+print("Dataframes test_df.pkl and train_df.pkl have been created in this directory")

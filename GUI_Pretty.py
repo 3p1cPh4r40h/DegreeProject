4ac1f590596e5528749dfb705e44d4efdc81baee
3p1cPh4r40h <64497655+3p1cPh4r40h@users.noreply.github.com>
2023-03-24 12:25:40 -0400

    Update GUI_Pretty.py

--- GUI_Pretty.py
+++ GUI_Pretty.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
 from GUI_Interface import GUI_Interface
-import numpy as np
-from PIL import Image, ImageTk
+from librosa import display
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 import winsound
+
+
 class GUI(tk.Frame):
 
     def __init__(self, master=None):
@@ -12,18 +17,18 @@ class GUI(tk.Frame):
         self.master = master
         self.master.title("Audio Processing GUI")
         self.gui_interface = GUI_Interface()
-        self.image = None
-        self.file_path = ""
-        self.master.configure(background='light blue')
-
         self.create_widgets()
+        self.file_path = ""
+        self.file_path2 = ""
+        self.file_path1 = ""
+        self.master.configure(background='light blue')
     def create_widgets(self):
 
         # Transcribe button and label
         self.transcribe_label = tk.Label(self.master, text="Transcribed Chords:",bg = "light blue")
         self.transcribe_button = tk.Button(self.master, text="Transcribe", command=self.get_transcribed_audio,fg="white",bg = "blue")
         self.play_button = tk.Button(self.master, text="Play Song", command=self.playMusic,fg="white",bg = "lime green")
-      
+    
         self.transcribe_label.pack(side="top", pady=10,ipadx=5)
         self.transcribe_button.pack(side="top", pady=5,ipadx=5)
         self.play_button.pack(side="top", pady=5,ipadx=5)
@@ -36,18 +41,28 @@ class GUI(tk.Frame):
         self.compare_button = tk.Button(self.master, text="Compare", command=self.get_compared_spectrograms,fg="white",bg = "blue")
         self.compare_label.pack(side="top", pady=10,ipadx=5,padx = 0)
         self.compare_button.pack(side="top", pady=5,ipadx=5,padx = 0)
+        
+        self.play_button1 = tk.Button(self.master, text="Play Song 1", command=self.playMusic1,fg="white",bg = "lime green")
+        self.play_button2 = tk.Button(self.master, text="Play Song 2", command=self.playMusic2,fg="white",bg = "lime green") 
 
-        # Output Image
-        self.output_text = tk.Text(self.master, height=10, state="disabled")
+        self.output_text = tk.Text(self.master, height=2, state="disabled")
         self.output_text.pack(side="top", pady=5)
-
+        self.play_button1.pack(side="top", pady=5,ipadx=5)
+        self.play_button2.pack(side="top", pady=5,ipadx=5)
+        # Matplotlib canvas
+        self.fig, self.ax = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
+        self.canvas.get_tk_widget().pack(side='bottom', fill='both', expand=True)
+        # Add navigation toolbar
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.master)
+        self.toolbar.update()
 
     def get_transcribed_audio(self):
 
         # Prompt user to select audio file
-        self.file_path  = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
-        
-        if self.file_path :
+        self.file_path = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+
+        if self.file_path:
 
             # Display loading message
             self.transcribe_output_text.config(state="normal") # set state to normal
@@ -57,7 +72,7 @@ class GUI(tk.Frame):
             self.transcribe_output_text.config(state="disabled") # set state back to disabled
 
             # Set audio1 in GUI_Interface
-            self.gui_interface.setAudio1(self.file_path )
+            self.gui_interface.setAudio1(self.file_path)
 
             # Get transcribed chords and display them
             chords = self.gui_interface.getTranscribedAudio()
@@ -65,14 +80,15 @@ class GUI(tk.Frame):
             self.transcribe_output_text.delete("1.0", tk.END)
             self.transcribe_output_text.insert(tk.END, " ".join(chords))
             self.transcribe_output_text.config(state="disabled") # set state back to disabled
+            
+            
 
     def get_compared_spectrograms(self):
-
         # Prompt user to select two audio files
-        file_path1 = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
-        if file_path1:
-            file_path2 = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
-            if file_path2:
+        self.file_path1 = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+        if self.file_path1:
+            self.file_path2 = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+            if self.file_path2:
 
                 # Display loading message
                 self.output_text.config(state="normal") # set state to normal
@@ -82,36 +98,40 @@ class GUI(tk.Frame):
                 self.output_text.config(state="disabled") # set state back to disabled
 
                 # Set audio1 and audio2 in GUI_Interface
-                self.gui_interface.setAudio1(file_path1)
-                self.gui_interface.setAudio2(file_path2)
+                self.gui_interface.setAudio1(self.file_path1)
+                self.gui_interface.setAudio2(self.file_path2)
 
                 # Get compared spectrograms and display them
                 spectrogram = self.gui_interface.getComparedSpectrograms()
+
+                # Plot the spectrogram using Matplotlib
+                display.specshow(spectrogram, ax=self.ax, x_axis='time', y_axis='linear')
+                self.ax.set(title='Compared Spectrograms')
+
+                # Draw the canvas
+                self.canvas.draw()
+                
+
+                # Update output text
                 self.output_text.config(state="normal") # set state to normal
                 self.output_text.delete("1.0", tk.END)
-                self.convert_to_image(spectrogram)
-                self.output_text.image_create(tk.END, image=self.image)
+                self.output_text.insert(tk.END, "Done")
                 self.output_text.config(state="disabled") # set state back to disabled
-        
-
-    def convert_to_image(self, spectrogram):
-
-        # Convert spectrogram to tkinter image
-        spectrogram *= 255
-        spectrogram = np.abs(255 - spectrogram)
-
-        # Convert the spectrogram to a PIL image
-        image = Image.fromarray(np.uint8(spectrogram)).convert("RGB")
-
-        # Convert the PIL image to a Tkinter image and return it
-        self.image = ImageTk.PhotoImage(image)
-
 
     def playMusic(self):
         # check if the user has entered in some music
         if self.file_path:
-            winsound.PlaySound(self.file_path, winsound.SND_FILENAME)
+            winsound.PlaySound(self.file_path, winsound.SND_FILENAME) 
 
+    def playMusic1(self):
+        # check if the user has entered in some music
+        if self.file_path1:
+            winsound.PlaySound(self.file_path1, winsound.SND_FILENAME) 
+
+    def playMusic2(self):
+        # check if the user has entered in some music
+        if self.file_path2:
+            winsound.PlaySound(self.file_path2, winsound.SND_FILENAME) 
 
 
 
